b049ba104dd18b6de6d9b7ca2f8ea599f0f4061b
KarimHamze <kariham9@gmail.com>
2024-03-23 17:44:15 -0300

    Highscore y cortar codigos

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 'use strict';
 
-let score = 20;
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -14,17 +15,23 @@ document.querySelector('.check').addEventListener('click', function () {
 
   // Cuando adivina
   else if (guess === secretNumber) {
+    //text
     document.querySelector('.message').textContent = '🥳 Numero correcto!';
+    document.querySelector('.number').textContent = secretNumber;
+    //style
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
-    document.querySelector('.number').textContent = secretNumber;
-    document.querySelector('.highscore').textContent = score;
+    //score
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = score;
+    }
   }
-
-  // Cuando el numero es muy alto
-  else if (guess > secretNumber) {
+  // Cuando el jugador no adivina
+  else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = '👆 Muy alto! ';
+      document.querySelector('.message').textContent =
+        guess > secretNumber ? '👆 Muy alto!' : '👇 Muy bajo!';
       score--;
       document.querySelector('.score').textContent = score;
     } else {
@@ -32,22 +39,27 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
       document.querySelector('body').style.backgroundColor = '#ff0000';
     }
-  }
 
-  // Cuando el numero es muy bajo
-  else if (guess < secretNumber) {
-    if (score > 0) {
-      document.querySelector('.message').textContent = '👇 Muy bajo! ';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = '😒 Perdiste! ';
-      document.querySelector('.score').textContent = 0;
-      document.querySelector('body').style.backgroundColor = '#ff0000';
-    }
+    // Cuando el numero es muy alto
+    // else if (guess > secretNumber) {
+    //   }
+    // }
+
+    // Cuando el numero es muy bajo
+    // else if (guess < secretNumber) {
+    //   if (score > 1) {
+    //     document.querySelector('.message').textContent = '👇 Muy bajo! ';
+    //     score--;
+    //     document.querySelector('.score').textContent = score;
+    //   } else {
+    //     document.querySelector('.message').textContent = '😒 Perdiste! ';
+    //     document.querySelector('.score').textContent = 0;
+    //     document.querySelector('body').style.backgroundColor = '#ff0000';
+    //   }
   }
 });
 
+// Resetear el Juego
 document.querySelector('.again').addEventListener('click', function () {
   // Score
   score = 20;
